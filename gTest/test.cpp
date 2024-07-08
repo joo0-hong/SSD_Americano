ed5d5f09bb3d502ee242b7db99f4649c62aeb173
junyoung <jun6ak@gmail.com>
2024-07-08 16:26:59 +0900

    [refact] filemanager test fixture 생성 및 파일 분리

--- gTest/test.cpp
+++ gTest/test.cpp
@@ -1,7 +1,6 @@
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
 #include "../SSD_Americano/NandInterface.h"
-#include "../SSD_Americano/FileManager.cpp"
 
 using namespace testing;
 
@@ -19,37 +18,4 @@ TEST(SSDTest, NANDInterface) {
 
     nand.read(0);
     nand.write(0, 0);
-}
-
-TEST(FileManagerTest, FileManager) {
-    FileManager* fileManager = new FileManager();
-
-    EXPECT_THAT(fileManager, NotNull());
-}
-
-TEST(FileManagerTest, FileManagerRead) {
-    FileManager* fileManager = new FileManager();
-    int offset = 0;
-    string data = "0x12341234";
-
-    try {
-        fileManager->read(offset);
-    }
-    catch (...) {
-        FAIL();
-    }
-}
-
-TEST(FileManagerTest, FileManagerWrite) {
-    FileManager* fileManager = new FileManager();
-    int offset = 0;
-    string data = "0x12341234";
-
-    try {
-        fileManager->write(offset, data);
-    }
-    catch (...) {
-        FAIL();
-    }
-
 }
