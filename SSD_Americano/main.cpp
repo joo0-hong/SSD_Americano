7e6f3d04b0cdb44c1ec31c835529d998fc018be1
jooyoung <hjoo820@naver.com>
2024-07-08 16:29:44 +0900

    Fixed conflicts

--- SSD_Americano/main.cpp
+++ SSD_Americano/main.cpp
@@ -1,20 +1,8 @@
 #include <stdio.h>
 #include <iostream>
-<<<<<<< HEAD
 using namespace std;
 
 int main(int argc, char* argv[])
 {	
-=======
-#include "hostInterface.cpp"
-using namespace std;
 
-
-int main(int argc, char* argv[])
-{
-	HostInterface hostIF;
-	hostIF.ParseCommand(argc, argv);
-	
->>>>>>> 503577d44a7fdd7288c68c2a90df9a49ab5ead26
-	return 0;
 }
