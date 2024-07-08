b1e836a0a834dbf718c6e6137662c73c588b52a5
taeung.yoon <ytw0201@gmail.com>
2024-07-08 14:51:45 +0900

    [feature] implement write method for LBA

--- TestShell_Americano/TestShell.cpp
+++ TestShell_Americano/TestShell.cpp
@@ -1,8 +1,13 @@
 #include <string>
 
+using namespace std;
+
 class TestShell {
 public:
 	void write(std::string lba, std::string data) {
+		int lba_val = std::stoi(lba);
+		if (lba_val < 0 || lba_val >= 100)
+			throw;
 	}
 	void read(std::string lba) {
 	}
--- TestShell_Americano_gTest/test.cpp
+++ TestShell_Americano_gTest/test.cpp
@@ -1,6 +1,32 @@
 #include "gmock/gmock.h"
+#include "../TestShell_Americano/TestShell.cpp"
+#include <string>
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+using namespace std;
+
+
+TEST(TestShell, TestShellWriteFail_LBA_largerThan100) {
+	TestShell ts;
+
+	string lba("100");
+	string data("0x1298CDEF");
+
+	ts.write(lba, data);
+
+	EXPECT_THROW(
+		{ ts.write(lba, data); }
+		,std::exception());
+}
+
+TEST(TestShell, TestShellWriteFail_LBA_lessThan0) {
+	TestShell ts;
+
+	string lba("-1");
+	string data("0x1298CDEF");
+
+	ts.write(lba, data);
+
+	EXPECT_THROW(
+		{ ts.write(lba, data); }
+	, std::exception());
 }
