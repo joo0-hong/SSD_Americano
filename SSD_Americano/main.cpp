b754a86f51875e5fcbcd618fff7f9ef4aec4d184
jooyoung <hjoo820@naver.com>
2024-07-08 16:26:24 +0900

    Reset main

--- SSD_Americano/main.cpp
+++ SSD_Americano/main.cpp
@@ -1,13 +1,8 @@
 #include <stdio.h>
 #include <iostream>
-#include "hostInterface.cpp"
 using namespace std;
 
-
 int main(int argc, char* argv[])
-{
-	HostInterface hostIF;
-	hostIF.ParseCommand(argc, argv);
-	
+{	
 	return 0;
 }
