102dad9c5dabfa8c9e39c31e5cec9b859e849386
hack.jang <hack.jang@samsung.com>
2024-07-08 16:47:27 +0900

    [choir] remove duplicated include directive

--- TestShell_Americano/TestShell.cpp
+++ TestShell_Americano/TestShell.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <iostream>
 
 using namespace std;
 
@@ -9,7 +8,7 @@ class TestShell {
 public:
 	TestShell(const std::string& ssd_path, const std::string& result_path)
 		: SSD_PATH(ssd_path), RESULT_PATH(result_path) {}
-	
+
 	void write(std::string lba, std::string data) {
 		string cmd("W");
 		string ret = SSD_PATH + " " + cmd + " " + lba + " " + data;
