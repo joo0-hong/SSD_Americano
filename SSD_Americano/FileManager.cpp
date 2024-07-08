c3acf5d173ab240ca880b358ec31bd02b50141a3
junyoung <jun6ak@gmail.com>
2024-07-08 17:10:15 +0900

    [feature] implement FileManager read
    (cherry picked from commit 2f94aebb9eea730485a83731eef2152603d5ccc4)

--- SSD_Americano/FileManager.cpp
+++ SSD_Americano/FileManager.cpp
@@ -1,16 +1,37 @@
 #include <string>
+#include <fstream>
+#include <vector>
+#include <iostream>
 
 using namespace std;
 
 class FileManager {
 public:
-    void read(int offset)
+    string read(int linenumber)
+    {
+        // TODO: Error handling of invalid linenumber
+        ifstream openFile(filename);
+        if (openFile.is_open()) {
+            string line;
+            int currentline = 0;
+
+            while (getline(openFile, line)) {
+                if (currentline == linenumber) {
+                    return line;
+                }
+                currentline++;
+            }
+            openFile.close();
+        }
+
+        return "";
+    }
+
+    void write(int linenumber, string data)
     {
         return;
     }
 
-    void write(int offset, string data)
-    {
-        return;
-    }
+private:
+    const string filename = "nand.txt";
 };
--- gTest/FileManagerTest.cpp
+++ gTest/FileManagerTest.cpp
@@ -2,6 +2,8 @@
 #include "gmock/gmock.h"
 #include "../SSD_Americano/FileManager.cpp"
 
+#include <fstream>
+
 using namespace testing;
 
 class FileManagerTestFixture : public testing::Test {
@@ -39,4 +41,23 @@ TEST_F(FileManagerTestFixture, FileManagerWrite) {
     catch (...) {
         FAIL();
     }
+}
+
+TEST_F(FileManagerTestFixture, FileManagerReadFileTest) {
+    string word_offset0 = "0x00000000";
+    string word_offset1 = "0xFFFFFFFF";
+    string testFileName = "nand.txt";
+    string data_out = "";
+
+    ofstream testFile(testFileName);
+    if (testFile.is_open()) {
+        testFile << word_offset0 << endl;
+        testFile << word_offset1 << endl;
+    }
+
+    data_out = fileManager->read(0);
+    EXPECT_EQ(word_offset0, data_out);
+
+    data_out = fileManager->read(1);
+    EXPECT_EQ(word_offset1, data_out);
 }
