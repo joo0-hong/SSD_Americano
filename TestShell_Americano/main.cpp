e8ba4329de02c522320a60f17ab257a3044b6342
hack.jang <hack.jang@samsung.com>
2024-07-08 14:13:36 +0900

    Add system() skeleton code

--- TestShell_Americano/main.cpp
+++ TestShell_Americano/main.cpp
@@ -1,3 +1,26 @@
+#include <iostream>
+#include <filesystem>
+#include <cstdlib>
+#include <string>
+
+//namespace fs = std::filesystem;
+
+using std::string;
+using std::cin;
+
 int main() {
+	string cmd, data, LBA;
+
+	system("cd");
+	//fs::path current_path = fs::current_path();
+
+
+	while (true) {
+		cin >> cmd >> LBA >> data;
+		string ret = "..\\x64\\Debug\\SSDMock " + cmd + " " + LBA + " " + data;
+
+		
+		system(ret.c_str());
+	}
 
 }
