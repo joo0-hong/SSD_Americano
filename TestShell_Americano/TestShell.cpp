80ad35e09561f88eafbf3566db24af977b487b82
hack.jang <hack.jang@samsung.com>
2024-07-08 16:35:17 +0900

    [feat] read

--- TestShell_Americano/TestShell.cpp
+++ TestShell_Americano/TestShell.cpp
@@ -1,10 +1,21 @@
+#include <iostream>
+#include <fstream>
 #include <string>
 
 class TestShell {
 public:
+	TestShell(const std::string& ssd_path, const std::string& result_path)
+		: SSD_PATH(ssd_path)
+	, RESULT_PATH(result_path) {}
+
 	void write(std::string lba, std::string data) {
 	}
 	void read(std::string lba) {
+		invokeSSDRead(lba);
+
+		std::string data = getSSDReadData();
+
+		std::cout << data << std::endl;
 	}
 	void exit() {
 	}
@@ -13,5 +24,26 @@ public:
 	void fullwrite() {
 	}
 	void fullread() {
+	}
+private:
+	const std::string SSD_PATH;
+	const std::string RESULT_PATH;
+
+	void invokeSSDRead(std::string& lba)
+	{
+		std::string cmd = SSD_PATH;
+		cmd += " ";
+		cmd += lba;
+		system(cmd.c_str());
+	}
+	std::string getSSDReadData() {
+		std::string result;
+
+		std::ifstream ifs;
+		ifs.open(RESULT_PATH);
+		ifs >> result;
+		ifs.close();
+
+		return result;
 	}
 };
--- TestShell_Americano_gTest/test.cpp
+++ TestShell_Americano_gTest/test.cpp
@@ -1,6 +1,15 @@
+#include <string>
+
 #include "gmock/gmock.h"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+#include "../TestShell_Americano/TestShell.cpp"
+
+TEST(TestShell, Read_InvalidCommand) {
+	const std::string SSD_PATH = "..\\x64\\Debug\\SSDMock";
+	const std::string RESULT_PATH = "..\\resources\\result.txt";
+	
+	TestShell app(SSD_PATH, RESULT_PATH);
+	
+	app.read("-1");
+	app.read("dsaf");
 }
