8dcc653be9edfad2a8077aefc6214b5cde1d34ff
hack.jang <hack.jang@samsung.com>
2024-07-09 15:33:19 +0900

    [refactor] add const keyword to argument

--- TestShell_Americano/SSDDriver.cpp
+++ TestShell_Americano/SSDDriver.cpp
@@ -3,11 +3,11 @@
 SSDDriver::SSDDriver(const string& ssdPath)
 	: ssdPath_{ ssdPath } {}
 
-void SSDDriver::write(string lba, string data) {
+void SSDDriver::write(const string&, const string& data) const {
 	
 }
 
-void SSDDriver::read(string lba) const {
+void SSDDriver::read(const string& lba) const {
 	string cmd("R");
 	string ret = ssdPath_ + " " + cmd + " " + lba;
 	system(ret.c_str());
