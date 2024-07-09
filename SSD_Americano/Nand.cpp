64900a4d11a9f13d9ee65aeb350ff9d2fdd16b79
joenggyun.yu <jeonggyun@gmail.com>
2024-07-09 12:13:39 +0900

    Change file name parameter

--- SSD_Americano/Nand.cpp
+++ SSD_Americano/Nand.cpp
@@ -9,26 +9,28 @@ using namespace std;
 
 class NAND : public NANDInterface {
 public:
+	NAND(string nandFile, string resultFile)
+	{
+		nandFileManager = new FileManager{ nandFile };
+		resultFileManager = new FileManager{ resultFile };
+	}
 	void read(int lba) override
 	{
-		string result = nandFileManager.read(lba);
-		resultFileManager.write(0, result);
+		string result = nandFileManager->read(lba);
+		resultFileManager->write(0, result);
 	}
 
 	void write(int lba, string data) override
 	{
-		nandFileManager.write(lba, data);
+		nandFileManager->write(lba, data);
 	}
 
 	void error() override
 	{
-		resultFileManager.write(0, "NULL");
+		resultFileManager->write(0, "NULL");
 	}
 
 private:
-	const string RESULT_FILE = "TestResult.txt";
-	const string NAND_FILE = "TestNand.txt";
-
-	FileManager nandFileManager{ NAND_FILE };
-	FileManager resultFileManager{ RESULT_FILE };
+	FileManager* nandFileManager;
+	FileManager* resultFileManager;
 };
