176262c22455b3d012a529b9ac61e0d5a267e8e2
taeung.yoon <ytw0201@gmail.com>
2024-07-09 11:07:40 +0900

    [feature] implement help fucntion

--- TestShell_Americano/TestShell.cpp
+++ TestShell_Americano/TestShell.cpp
@@ -23,6 +23,12 @@ public:
 	void exit() {
 	}
 	void help() {
+		helpWrite();
+		helpRead();
+		helpFullWrite();
+		helpFullRead();
+		helpHelp();
+		helpExit();
 	}
 	void fullwrite(std::string data) {
 		for (int lba = 0; lba < 100; lba++) {
@@ -54,5 +60,73 @@ private:
 		ifs.close();
 
 		return result;
+	} 
+	void helpExit() const
+	{
+		std::cout << "======================================================" << std::endl;
+		std::cout << "[NAME]" << std::endl;
+		std::cout << "exit" << std::endl << std::endl;
+		std::cout << "[SYNOPSIS]" << std::endl;
+		std::cout << "- exit []" << std::endl << std::endl;
+		std::cout << "[DESCRIPTION]" << std::endl;
+		std::cout << "- exit the Test Shell" << std::endl;
+		std::cout << "======================================================" << std::endl << std::endl;
+	}
+	void helpHelp() const
+	{
+		std::cout << "======================================================" << std::endl;
+		std::cout << "[NAME]" << std::endl;
+		std::cout << "help" << std::endl << std::endl;
+		std::cout << "[SYNOPSIS]" << std::endl;
+		std::cout << "- help []" << std::endl << std::endl;
+		std::cout << "[DESCRIPTION]" << std::endl;
+		std::cout << "- dispaly help information about the Test Shell" << std::endl;
+		std::cout << "======================================================" << std::endl << std::endl;
+	}
+	void helpFullRead() const
+	{
+		std::cout << "======================================================" << std::endl;
+		std::cout << "[NAME]" << std::endl;
+		std::cout << "fullread" << std::endl << std::endl;
+		std::cout << "[SYNOPSIS]" << std::endl;
+		std::cout << "- fullread []" << std::endl << std::endl;
+		std::cout << "[DESCRIPTION]" << std::endl;
+		std::cout << "- read data from LBA #0 to #99" << std::endl;
+		std::cout << "======================================================" << std::endl << std::endl;
+	}
+	void helpFullWrite() const
+	{
+		std::cout << "======================================================" << std::endl;
+		std::cout << "[NAME]" << std::endl;
+		std::cout << "fullwrite" << std::endl << std::endl;
+		std::cout << "[SYNOPSIS]" << std::endl;
+		std::cout << "- fullwrite [DATA]" << std::endl << std::endl;
+		std::cout << "[DESCRIPTION]" << std::endl;
+		std::cout << "- write data from LBA #0 to #99" << std::endl;
+		std::cout << "======================================================" << std::endl << std::endl;
+	}
+	void helpRead() const
+	{
+		std::cout << "======================================================" << std::endl;
+		std::cout << "[NAME]" << std::endl;
+		std::cout << "read" << std::endl;
+		std::cout << "[SYNOPSIS]" << std::endl;
+		std::cout << "- read [LBA]" << std::endl;
+		std::cout << "[DESCRIPTION]" << std::endl;
+		std::cout << "- read data from LBA" << std::endl;
+		std::cout << "======================================================" << std::endl << std::endl;
+
+	}
+	void helpWrite() const
+	{
+		std::cout << "======================================================" << std::endl;
+		std::cout << "[NAME]" << std::endl;
+		std::cout << "write" << std::endl << std::endl;
+		std::cout << "[SYNOPSIS]" << std::endl;
+		std::cout << "- write [LBA] [DATA]" << std::endl << std::endl;
+		std::cout << "[DESCRIPTION]" << std::endl;
+		std::cout << "- write data to LBA" << std::endl;
+		std::cout << "======================================================" << std::endl << std::endl;
+
 	}
 };
