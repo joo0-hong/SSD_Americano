aa7add1c9f8d638c0cf0af06f50b4a31b6571702
jooyoung <hjoo820@naver.com>
2024-07-10 17:48:38 +0900

    [Feature] Fixed string formats and checking size

--- Logger/Logger.h
+++ Logger/Logger.h
@@ -11,5 +11,7 @@ public:
 private:
 	static string GetUntilFileName(void);
 	static bool CheckFileOpen(std::fstream& file);
+	static void WriteToLatestLog(const string& funcName, const string& content, fstream& file);
+	static bool isExceedMaxFileSize(std::fstream& file);
 	static const int MAX_FILE_SIZE = 10 * 1024;
 };
