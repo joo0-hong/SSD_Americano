4f350fab16aa79dfa3a2a23675c80eafcf27895b
joenggyun.yu <jeonggyun@gmail.com>
2024-07-10 14:09:29 +0900

    reordering HostInterface functions

--- SSD_Americano/HostInterface.cpp
+++ SSD_Americano/HostInterface.cpp
@@ -4,6 +4,53 @@
 #include "ReadCmd.h"
 #include "EraseCmd.h"
 
+void HostInterface::processCommand(int argc, char* argv[])
+{
+	Command* cmd = nullptr;
+	bool isInvalidCmd = checkInvalidCommand(argc, argv);
+
+	if (isInvalidCmd == true) {
+		cmd = new ErrorCmd(nandIntf);
+	}
+	else if (string(argv[1]) == "W") {
+		addr = atoi(argv[2]);
+		data = string(argv[3]);
+		cmd = new WriteCmd(addr, data, nandIntf);
+	}
+	else if (string(argv[1]) == "R") {
+		addr = atoi(argv[2]);
+		cmd = new ReadCmd(addr, nandIntf);
+	}
+	else if (string(argv[1]) == "E") {
+		addr = atoi(argv[2]);
+		size = string(argv[3]);
+		cmd = new EraseCmd(addr, size, nandIntf);
+	}
+
+	cmd->run();
+	delete cmd;
+}
+
+bool HostInterface::checkInvalidCommand(int argc, char* argv[]) {
+	if (checkInvalidArguementNumber(argc, argv)) {
+		return true;
+	}
+
+	if (checkInvalidLBA(argv)) {
+		return true;
+	}
+
+	if (checkInvalidDataToWrite(argv)) {
+		return true;
+	}
+
+	if (checkEraseSize(argv)) {
+		return true;
+	}
+
+	return false;
+}
+
 bool HostInterface::checkInvalidArguementNumber(int argc, char* argv[])
 {
 	if (argc < MIN_VALID_ARGUMENT_NUM) {
@@ -29,46 +76,26 @@ bool HostInterface::checkInvalidArguementNumber(int argc, char* argv[])
 	return false;
 }
 
-bool HostInterface::checkInvalidCommand(int argc, char* argv[]) {
-	if (checkInvalidArguementNumber(argc, argv)) {
-		return true;
-	}
-
-	if (checkInvalidLBA(argv)) {
-		return true;
-	}
-
-	if (checkInvalidDataToWrite(argv)) {
-		return true;
-	}
-
-	if (checkEraseSize(argv)) {
-		return true;
-	}
-
-	return false;
-}
-
-bool HostInterface::checkEraseSize(char* argv[])
+bool HostInterface::checkInvalidLBA(char* argv[])
 {
-	if (string(argv[1]) == "E") {
-		for (const char ch : string(argv[3])) {
+	if (string(argv[1]) == "R" || string(argv[1]) == "W" || string(argv[1]) == "E") {
+		for (const char ch : string(argv[2])) {
 			if ((ch >= '0') && (ch <= '9')) {
 				continue;
 			}
-			cout << "Invalid erase size !!!" << endl;
+			cout << "Invalid LBA !!!" << endl;
 			return true;
 		}
-
-		int eraseSize = stoi(string(argv[3]));
-		if (eraseSize <= 0 && eraseSize > MAX_ERASE_SIZE) {
-			cout << "Invalid erase size !!!" << endl;
+		int lba = atoi(argv[2]);
+		if (lba < MIN_LBA || lba > MAX_LBA) {
+			cout << "LBA is out of range !!!" << endl;
 			return true;
 		}
 	}
 
 	return false;
 }
+
 
 bool HostInterface::checkInvalidDataToWrite(char* argv[])
 {
@@ -101,51 +128,25 @@ bool HostInterface::checkInvalidDataToWrite(char* argv[])
 	return false;
 }
 
-bool HostInterface::checkInvalidLBA(char* argv[])
+bool HostInterface::checkEraseSize(char* argv[])
 {
-	if (string(argv[1]) == "R" || string(argv[1]) == "W" || string(argv[1]) == "E") {
-		for (const char ch : string(argv[2])) {
+	if (string(argv[1]) == "E") {
+		for (const char ch : string(argv[3])) {
 			if ((ch >= '0') && (ch <= '9')) {
 				continue;
 			}
-			cout << "Invalid LBA !!!" << endl;
+			cout << "Invalid erase size !!!" << endl;
 			return true;
 		}
-		int lba = atoi(argv[2]);
-		if (lba < MIN_LBA || lba > MAX_LBA) {
-			cout << "LBA is out of range !!!" << endl;
+
+		int eraseSize = stoi(string(argv[3]));
+		if (eraseSize <= 0 && eraseSize > MAX_ERASE_SIZE) {
+			cout << "Invalid erase size !!!" << endl;
 			return true;
 		}
 	}
 
 	return false;
-}
-
-void HostInterface::processCommand(int argc, char* argv[])
-{
-	Command* cmd = nullptr;
-	bool isInvalidCmd = checkInvalidCommand(argc, argv);
-
-	if (isInvalidCmd == true) {
-		cmd = new ErrorCmd(nandIntf);
-	}
-	else if (string(argv[1]) == "W") {
-		addr = atoi(argv[2]);
-		data = string(argv[3]);
-		cmd = new WriteCmd(addr, data, nandIntf);
-	}
-	else if (string(argv[1]) == "R") {
-		addr = atoi(argv[2]);
-		cmd = new ReadCmd(addr, nandIntf);
-	}
-	else if (string(argv[1]) == "E") {
-		addr = atoi(argv[2]);
-		size = string(argv[3]);
-		cmd = new EraseCmd(addr, size, nandIntf);
-	}
-
-	cmd->run();
-	delete cmd;
 }
 
 inline int HostInterface::getCmd() {
