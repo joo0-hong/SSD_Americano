6f6bc17f547e6af1535434d17dda191b07ca8401
qhcjfdudn <jhjhyjin@naver.com>
2024-07-11 13:47:31 +0900

    [Init] test code

---
+++ TestShell_Americano/ScenarioParser.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <string>
+
+#include <rapidjson/document.h>
+
+class ScenarioParser
+{
+public:
+	static ScenarioParser& getInstance();
+	void test();
+private:
+	const std::string TEST_SCENARIO_PATH = "..\\resources\\test_scenario.json";
+	
+	ScenarioParser() = default;
+	ScenarioParser(const ScenarioParser&) = delete;
+	ScenarioParser& operator=(const ScenarioParser&) = delete;
+	
+	bool setDocument(rapidjson::Document& d);
+
+	bool checkValid(std::ifstream& file);
+	
+	std::string getStr(std::ifstream& file);
+	
+	void printParsingErrorMsg();
+};
+
