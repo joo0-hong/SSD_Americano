ce38b55b555dc96a59e098b00a9d72d283ebc06f
qhcjfdudn <jhjhyjin@naver.com>
2024-07-11 17:44:18 +0900

    [modify] change return type of test method

--- TestShell_Americano/ScenarioParser.h
+++ TestShell_Americano/ScenarioParser.h
@@ -9,7 +9,7 @@ class ScenarioParser
 {
 public:
 	static ScenarioParser& getInstance();
-	std::pair<std::vector<std::string>, std::vector<std::string>> test();
+	std::pair<std::vector<std::string>, std::vector<std::vector<std::string>>> test();
 private:
 	const std::string TEST_SCENARIO_PATH = "..\\resources\\test_scenario.json";
 	
@@ -20,6 +20,7 @@ private:
 	bool setDocument(rapidjson::Document& d);
 
 	bool checkValid(std::ifstream& file);
+	bool checkDocument(rapidjson::Document& document);
 	
 	std::string getWriteInputString(rapidjson::Value& actionJson, int lba);
 	std::string getReadInputString(rapidjson::Value& actionJson, int lba);
